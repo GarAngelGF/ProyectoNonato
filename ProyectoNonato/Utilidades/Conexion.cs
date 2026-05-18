8db5bc05d79788adbd5cb34e23826f73c5f21fc6
Angel <agarciaf2102@alumno.ipn.mx>
2026-05-18 10:41:21 -0600

    arreglo

--- ProyectoNonato/Utilidades/Conexion.cs
+++ ProyectoNonato/Utilidades/Conexion.cs
@@ -6,8 +6,8 @@ namespace ProyectoNonato.Utilidades
     public class Conexion
     {
         // Nota: Reemplaza "TuPassword" con la contraseña de adminprobd2026 definida en Azure
-        // public static string CadenaSQLBase = "Server=tcp:adminbdnonato26.database.windows.net,1433;Initial Catalog=EscuelaDBPADMBD;Persist Security Info=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-        public static string CadenaSQLBase = "Server=.\\SQLEX;Initial Catalog=EscuelaDBPADMBD;TrustServerCertificate=True;";
+        public static string CadenaSQLBase = "Server=tcp:adminbdnonato26.database.windows.net,1433;Initial Catalog=EscuelaDBPADMBD;Persist Security Info=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+       // public static string CadenaSQLBase = "Server=.\\SQLEX;Initial Catalog=EscuelaDBPADMBD;TrustServerCertificate=True;";
 
         public static string GenerarCadenaDinamica(string user, string pass)
         {
